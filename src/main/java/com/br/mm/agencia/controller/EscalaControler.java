39457ed7c152c55748c1490277ce88493116fc7b
Maurilio Menezes <mauriliosm25@gmail.com>
2022-10-24 20:08:33 -0300

    feat: cadastrar escalas

--- src/main/java/com/br/mm/agencia/controller/EscalaControler.java
+++ src/main/java/com/br/mm/agencia/controller/EscalaControler.java
@@ -1,16 +1,23 @@
 package com.br.mm.agencia.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.br.mm.agencia.controller.form.EscalaForm;
 import com.br.mm.agencia.model.Escala;
 import com.br.mm.agencia.repository.EscalaRepository;
+
+import net.bytebuddy.asm.Advice.Return;
 
 @Controller
 @ResponseBody
@@ -26,6 +33,17 @@ public class EscalaControler {
     List<Escala> list = escalaRepository.findByOrigem(origem);
 
     return list;
+  }
+
+  @PostMapping("/cadastrar")
+  public ResponseEntity<?> cadastrar(@RequestBody EscalaForm form) {
+
+    Escala escala = new Escala(form.getOrigem(), form.getDestino());
+
+    escalaRepository.save(escala);
+
+    return ResponseEntity.ok().body(escala);
+
   }
 
 }
---
+++ src/main/java/com/br/mm/agencia/controller/form/EscalaForm.java
@@ -0,0 +1,26 @@
+package com.br.mm.agencia.controller.form;
+
+import java.time.LocalDateTime;
+
+public class EscalaForm {
+
+  public String origem;
+  public String destino;
+
+  public String getOrigem() {
+    return this.origem;
+  }
+
+  public void setOrigem(String origem) {
+    this.origem = origem;
+  }
+
+  public String getDestino() {
+    return this.destino;
+  }
+
+  public void setDestino(String destino) {
+    this.destino = destino;
+  }
+
+}
